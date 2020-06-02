9964c5da4b707eab48f72ec009451450693853fb
aklefebvere <aklefebvere@gmail.com>
2020-06-02 16:34:12 -0400

    testing AWS processing time

--- main.py
+++ main.py
@@ -65,8 +65,9 @@ def transaction(full_dict: dict):
     trans = TransactionHistory(full_dict=full_dict)
     user_id = full_dict['user_id']
     request = trans.getCats(cats_dict=getUser(user_id))
-    print("--- %s seconds ---" % (time.time() - start_time))
-    return request
+    # print("--- %s seconds ---" % (time.time() - start_time))
+    final_time = time.time() - start_time
+    return final_time
 
 @app.get("/admin/reset_master/")
 def reset_master(username: str = Depends(get_current_username)):
