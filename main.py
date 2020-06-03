261506d7ffca960480eab90b269a740c1b649d77
aklefebvere <aklefebvere@gmail.com>
2020-06-03 14:22:37 -0400

    added login params

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ security = HTTPBasic()
 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "test")
-    correct_password = secrets.compare_digest(credentials.password, "test")
+    correct_username = secrets.compare_digest(credentials.username, AUSERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
     if not (correct_username and correct_password):
         raise HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
